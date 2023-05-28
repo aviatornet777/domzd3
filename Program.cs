3b2f56336b4bb27a65b30c2c68f52ab0a8325c93
Serg <Serg.srav@mail.ru>
2023-05-28 22:26:37 +0500

    домашнее задание 3

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿        // Задача №1
+
+        //Console.WriteLine("Введите пятизначное число");
+        //int num = Convert.ToInt32(Console.ReadLine());
+
+        //int a=num/10000;
+        //int b=num%10000/1000;
+        //int c=num%1000/100;
+        //int d=num%100/10;
+        //int e=num%10;
+
+        //if((a==e) && (b==d))
+        //{
+        //    Console.WriteLine("Данное число является полиндромом");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Данное число не полиндром");
+        //}
+
+
+//          Задача2
+//Console.WriteLine("Введите число");
+//int X1 = Convert.ToInt32(Console.ReadLine());    
+
+//Console.WriteLine("Введите число");
+//int Y1 = Convert.ToInt32(Console.ReadLine()); 
+
+//Console.WriteLine("Введите число");
+//int Z1 = Convert.ToInt32(Console.ReadLine());
+
+
+//Console.WriteLine("Введите число");
+//int X2 = Convert.ToInt32(Console.ReadLine()); 
+
+//Console.WriteLine("Введите число");
+//int Y2 = Convert.ToInt32(Console.ReadLine()); 
+
+//Console.WriteLine("Введите число");
+//int Z2 = Convert.ToInt32(Console.ReadLine());
+
+//Double result = Math.Sqrt(Math.Pow((X2-X1),2) + Math.Pow((Y2-Y1),2) + Math.Pow((Z2-Z1),2));
+
+//Console.WriteLine(result);
+
+
+
+//          Задача №3
+
+//Console.WriteLine("Введит число");
+//int a = Convert.ToInt32(Console.ReadLine());
+
+//int count = 1;
+
+  //  while(a>=count)
+    //{
+      //  Console.WriteLine(Math.Pow(count,3));
+        //count++;
+    //}
